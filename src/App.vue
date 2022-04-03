33f539ffc9aabce994eb1d76a8e46f3edfd62ca3
RileyHal <halbertriley@gmail.com>
2022-04-02 20:28:57 -0400

    Added front end validation

--- src/App.vue
+++ src/App.vue
@@ -65,8 +65,10 @@
 </script>
 
 <template>
-  <div class = "data-container">
+  <!--This iframe stops the form redirect-->
+  <iframe name="dummyframe" id="dummyframe" style="display: none;"></iframe>
 
+  <div class = "data-container">
     <div class="data-display">
       <p>Soil Moisture: {{parseFloat(state.data.soilMoisture*100).toFixed(2)}}%</p>
       <p>Is empty: {{ state.data.isEmpty}}</p>
@@ -75,24 +77,27 @@
       <p>Temperature: {{state.data.temperature}} &#8457;</p>
       <p>Pump Duration: {{state.data.pumpDuration}} seconds</p>
       <p>Pump Interval: {{state.data.pumpInterval}} seconds</p>
+      <p>Mode: {{state.data.mode}}</p>
     </div>
 
-    <div id="input-container">
+    <form action='/' class="input-container" @submit=postData() target="dummyframe">
       <div class="input-item-container">
         <label>Pump Duration:</label>
-        <input class="textInput" type="text" v-model="input.pumpDuration" placeholder="Duration">
+        <input class="textInput" type="text" v-model="input.pumpDuration" placeholder="Duration" required>
       </div>
       <div class="input-item-container">
         <label>Pump Interval:</label>
-        <input class="textInput" type="text" v-model="input.pumpInterval" placeholder="Interval">
+        <input class="textInput" type="text" v-model="input.pumpInterval" placeholder="Interval" required>
       </div>
       <div class="input-item-container">
         <label>Mode:</label>
-        <input class="textInput" type="text" v-model="input.mode" placeholder="Mode">
+        <input class="textInput" type="text" v-model="input.mode" placeholder="Mode" required>
       </div>
-      <button v-on:click=postData()>Submit</button>
-    </div>
-    <div>Received Data: {{state.postData}}</div>
+      <input type="submit">
+    </form>
+
+    <p>Received Data: {{state.postData}}</p>
+
   </div>
 
 </template>
