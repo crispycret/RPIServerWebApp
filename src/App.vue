15c26aaf588a10c7908ac6ce72458b3e7425e5ad
RileyHal <halbertriley@gmail.com>
2022-04-03 02:37:06 -0400

    Updated UI

--- src/App.vue
+++ src/App.vue
@@ -65,42 +65,72 @@
 </script>
 
 <template>
-  <div class = "data-container">
-    <div class="data-display">
-      <button v-on:click=fetchData()>Refresh</button>
-      <p>Soil Moisture: {{parseFloat(state.data.soilMoisture*100).toFixed(2)}}%</p>
-      <p>Is empty: {{ state.data.isEmpty}}</p>
-      <p>Light Level: {{parseFloat(state.data.lightLevel*100).toFixed(2)}}%</p>
-      <p>Pump Active: {{ state.data.pumpActive }}</p>
-      <p>Temperature: {{state.data.temperature}} &#8457;</p>
-      <p>Pump Duration: {{state.data.pumpDuration}} seconds</p>
-      <p>Pump Interval: {{state.data.pumpInterval}} seconds</p>
-      <p>Mode: {{state.data.mode}}</p>
-    </div>
-    
-    <!--This iframe stops the form redirect-->
-    <iframe name="dummyframe" id="dummyframe" style="display: none;"></iframe>
-
-    <form action='/' class="input-container" @submit=postData() target="dummyframe">
-      <div class="input-item-container">
-        <label>Pump Duration:</label>
-        <input class="textInput" type="text" v-model="input.pumpDuration" placeholder="Duration" required>
-      </div>
-      <div class="input-item-container">
-        <label>Pump Interval:</label>
-        <input class="textInput" type="text" v-model="input.pumpInterval" placeholder="Interval" required>
-      </div>
-      <div class="input-item-container">
-        <label>Mode:</label>
-        <input class="textInput" type="text" v-model="input.mode" placeholder="Mode" required>
-      </div>
-      <input type="submit">
-    </form>
-
-    <p>Received Data: {{state.postData}}</p>
-
+  <div class="navbar">
+    <h1 class="nav-title">PlantWaterer</h1>
+    <div class="nav-item" id="nav-item-selected">Dashboard</div>
+    <div class="nav-item">Option 2</div>
+    <div class="nav-item">Option 3</div>
+    <div class="nav-item">Option 4</div>
   </div>
-
+  <div class="grid-container">
+    <div class="grid">
+      <div class="grid-item">
+        <div class="grid-item-title">Soil Moisture</div>
+        <div class="grid-item-data">{{parseFloat(state.data.soilMoisture*100).toFixed(2)}}%</div>
+      </div>
+      <div class="grid-item">
+        <div class="grid-item-title">Is empty</div>
+        <div class="grid-item-data">{{state.data.isEmpty}}</div>
+      </div>
+      <div class="grid-item">
+        <div class="grid-item-title">Light Level</div>
+        <div class="grid-item-data">{{parseFloat(state.data.lightLevel*100).toFixed(2)}}%</div>
+      </div>
+      <div class="grid-item">
+        <div class="grid-item-title">Pump Active</div>
+        <div class="grid-item-data">{{ state.data.pumpActive }}</div>
+      </div>
+      <div class="grid-item">
+        <div class="grid-item-title">Temperature</div>
+        <div class="grid-item-data">{{state.data.temperature}} &#8457;</div>
+      </div>
+      <div class="grid-item">
+        <div class="grid-item-title">Pump Duration</div>
+        <div class="grid-item-data">{{state.data.pumpDuration}} seconds</div>
+      </div>
+      <div class="grid-item">
+        <div class="grid-item-title">Pump Interval</div>
+        <div class="grid-item-data">{{state.data.pumpInterval}} seconds</div>
+      </div>
+      <div class="grid-item">
+        <div class="grid-item-title">Mode</div>
+        <div class="grid-item-data">{{state.data.mode}}</div>
+      </div>
+      <div></div>
+      <div class="grid-item">
+        <form action='' class="input-container" v-on:submit.prevent="onSubmit" @submit=postData()>
+          <div class="input-item-container">
+            <label>Pump Duration:</label>
+            <input class="text-input" type="text" v-model="input.pumpDuration" placeholder="Duration" required>
+          </div>
+          <div class="input-item-container">
+            <label>Pump Interval:</label>
+            <input class="text-input" type="text" v-model="input.pumpInterval" placeholder="Interval" required>
+          </div>
+          <div class="input-item-container">
+            <label>Mode:</label>
+            <input class="text-input" type="text" v-model="input.mode" placeholder="Mode" required>
+          </div>
+          <input type="submit" class="input-button">
+        </form>
+      </div>
+      <div class="grid-item">
+        <div class="grid-item-title">Received Data</div>
+        <div class="grid-item-data">{{state.postData}}</div>
+      </div>
+    </div>
+  </div>
+  
 </template>
 
 <style>
