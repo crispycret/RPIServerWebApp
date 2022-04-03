c892bc1f87d51fbbb5f21aa43c8d63a4c7643bbe
RileyHal <halbertriley@gmail.com>
2022-04-02 20:10:58 -0400

    Cleaned Code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
   import { ref, reactive } from "vue";
-  import Url from 'url'
 
   const state = reactive({
     status: '',
@@ -65,30 +64,28 @@
   }
 </script>
 
-
-
 <template>
-  <div>
-    <div class = "dataPoint"> Soil Moisture: {{parseFloat(state.data.soilMoisture*100).toFixed(2)}}%</div>
-    <div class = "dataPoint"> Is empty: {{ state.data.isEmpty}}</div>
-    <div class = "dataPoint"> Light Level: {{parseFloat(state.data.lightLevel*100).toFixed(2)}}%</div>
-    <div class = "dataPoint"> Pump Active: {{ state.data.pumpActive }}</div>
-    <div class = "dataPoint"> Temperature: {{state.data.temperature}} &#8457;</div>
-    <div class = "dataPoint">Pump Duration: {{state.data.pumpDuration}} seconds</div>
-    <div class = "dataPoint">Pump Interval: {{state.data.pumpInterval}} seconds</div>
+  <div class = "dataContainer">
+    <p>Soil Moisture: {{parseFloat(state.data.soilMoisture*100).toFixed(2)}}%</p>
+    <p>Is empty: {{ state.data.isEmpty}}</p>
+    <p>Light Level: {{parseFloat(state.data.lightLevel*100).toFixed(2)}}%</p>
+    <p>Pump Active: {{ state.data.pumpActive }}</p>
+    <p>Temperature: {{state.data.temperature}} &#8457;</p>
+    <p>Pump Duration: {{state.data.pumpDuration}} seconds</p>
+    <p>Pump Interval: {{state.data.pumpInterval}} seconds</p>
 
     <div id="inputContainer">
       <div class="inputItemContainer">
         <label for="pumpDuration">Pump Duration: </label>
-        <input class="textInput" id="pumpDuration" type="text" v-model="input.pumpDuration" placeholder="Duration">
+        <input class="textInput" type="text" v-model="input.pumpDuration" placeholder="Duration">
       </div>
       <div class="inputItemContainer">
         <label for="pumpInterval">Pump Interval: </label>
-        <input class="textInput" id="pumpInterval" type="text" v-model="input.pumpInterval" placeholder="Interval">
+        <input class="textInput" type="text" v-model="input.pumpInterval" placeholder="Interval">
       </div>
       <div class="inputItemContainer">
         <label for="mode">Mode:</label>
-        <input class="textInput" id='mode' type="text" v-model="input.mode" placeholder="Mode">
+        <input class="textInput" type="text" v-model="input.mode" placeholder="Mode">
       </div>
       <button v-on:click=postData()>Submit</button>
     </div>
@@ -98,35 +95,4 @@
 
 <style>
   @import "./assets/base.css";
-  #app {
-  font-weight: normal;
-  padding: 2rem;
-  display: flex;
-  justify-content: center;
-
-  text-align: center;
-  background-color: #8e9dbd;
-  color: #0f4061;
-  }
-
-  #inputContainer{
-    display:flex;
-    flex-direction: column;
-  }
-
-  .textInput{
-    margin-left: 30px;
-  }
-
-  .inputItemContainer{
-    padding: 0px 10px;
-    display:flex;
-    justify-content: space-between;
-    padding: 10px 0px;
-  }
-
-  .dataPoint {
-    color: 'black'
-  }
-
 </style>
